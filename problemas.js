06434d556918f0e4a77cda6dbde1fcd192eea23e
javisaravia <fso0001@alu.medac.es>
2025-10-07 09:24:44 +0200

    Add files via upload

---
+++ problemas.js
@@ -0,0 +1,29 @@
+
+//declaramos una variable
+
+let valor = 1;
+
+//convertimos a numero 
+let numero= Number(valor);
+
+//convertimos a booleano
+let booleano= Boolean(valor);
+
+//convertimos a texto
+let texto= String(valor);
+
+
+console.log(typeof valor, valor);       // string "123"
+console.log(typeof numero, numero);     // number 123
+console.log(typeof texto, texto);       // string "123"
+console.log(typeof booleano, booleano); // boolean true
+
+
+
+//Crea un script que concatene cadenas y números
+
+const nombre = "Javi";
+const edad = 20;
+ const mensaje = "Hola,  me llamo " + nombre + " y tengo " + edad + " años. "
+ 
+ console.log(mensaje);
